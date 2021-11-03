09fc6d6a65935199e9caba453275f3a31992b33e
markfilliph <71455987+markfilliph@users.noreply.github.com>
2021-11-03 12:13:22 -0400

    Chapter 11 - page314 - could not check the anchor preset

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
     private static Game singleton;
-    [SerializeField]
-    RobotSpawn[] spawns;
+    [SerializeField] RobotSpawn[] spawns;
     public int enemiesLeft;
 
     public GameUI gameUI;
     public GameObject player;
     public int score;
     public int waveCountdown;
+
     public bool isGameOver;
+
     // 1
     void Start()
     {
         singleton = this;
+        StartCoroutine("increaseScoreEachSecond");
+        isGameOver = false;
+        Time.timeScale = 1;
+        waveCountdown = 30;
+        enemiesLeft = 0;
+        StartCoroutine("updateWaveTimer");
         SpawnRobots();
     }
+
     // 2
     private void SpawnRobots()
     {
@@ -29,12 +38,59 @@ public class Game : MonoBehaviour
             spawn.SpawnRobot();
             enemiesLeft++;
         }
+        gameUI.SetEnemyText(enemiesLeft);
     }
-    
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private IEnumerator updateWaveTimer()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1f);
+            waveCountdown--;
+            gameUI.SetWaveText(waveCountdown);
+            // Spawn next wave and restart count down
+            if (waveCountdown == 0)
+            {
+                SpawnRobots();
+                waveCountdown = 30;
+                gameUI.ShowNewWaveText();
+            }
+        }
+    }
+
+    public static void RemoveEnemy()
+    {
+        singleton.enemiesLeft--;
+        singleton.gameUI.SetEnemyText(singleton.enemiesLeft);
+        // Give player bonus for clearing the wave before timer is done
+        if (singleton.enemiesLeft == 0)
+        {
+            singleton.score += 50;
+            singleton.gameUI.ShowWaveClearBonus();
+        }
+    }
+
+    public void AddRobotKillToScore()
+    {
+        score += 10;
+        gameUI.SetScoreText(score);
+    }
+
+    IEnumerator increaseScoreEachSecond()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1);
+            score += 1;
+            gameUI.SetScoreText(score);
+        }
     }
 }
